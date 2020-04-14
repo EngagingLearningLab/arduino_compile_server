a852c740b4d32154ed150c5fc76cdd4e613611df
nionata <n.ionata129@gmail.com>
2020-04-14 18:19:30 -0400

    updated the package and app file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const config = require('./config.json');
 const port = !isNaN(Number(process.argv[2])) ? Number(process.argv[2]) : config.port;
-const {version} = require('./package.json');
+const { name , version } = require('./package.json');
 const arduino_dir = __dirname + "/arduino-1.8.5";
 const arduino = new (require("./arduino.js"))(arduino_dir);
 
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("trust proxy", 1); // trust first proxy
 
-app.get('/version', (req, res) => res.json({version, program: "chromeduino"}));
+app.get('/version', (req, res) => res.json({name, version}));
 
 app.get('/boards', (req, res) => res.json(arduino.boards));
 
@@ -29,10 +29,5 @@ app.post('/compile', (req, res) => {
         return res.json({success: false, msg: "invalid parameters passed"});
     arduino.compile(req.body.sketch, req.body.board).catch(console.error).then(data=>res.json(data));
 });
-
-
-
-
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
